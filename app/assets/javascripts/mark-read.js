330fa236e78d623a0b63321bad3ea56023ae7e66
SusiIrwin <susi.irwin@gmail.com>
2017-01-09 16:42:03 -0700

    adjust js

--- app/assets/javascripts/mark-read.js
+++ app/assets/javascripts/mark-read.js
@@ -2,7 +2,7 @@ var $newLinkTitle, $newLinkUrl;
 
 $(document).ready(function(){
   console.log('loaded')
-  
+
 
   $('#links-list').on('click', 'button.mark-read', function(){
     console.log('win')
@@ -14,7 +14,6 @@ $(document).ready(function(){
     //   method: 'PATCH',
     //   data: {read: true}
     // });
-  // })
 
   var readLink = $(this).data("url")
 
@@ -22,5 +21,6 @@ $(document).ready(function(){
     method: 'POST',
     data: {url: readLink},
     url: "https://si-m4-final-service.herokuapp.com/links"
+  })
   })
 })
