b1b9dda64c34f9f400507dc11a0e76e4aba2bd44
SusiIrwin <susi.irwin@gmail.com>
2017-01-09 09:49:16 -0700

    Add test for logout

--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -17,4 +17,25 @@ RSpec.describe "can create login account", :js => :true do
     expect(page).to have_content("Logout")
 
   end
+
+  scenario "allows a registered user to Log Out of the Site" do
+    user = User.create(email: "email@email.com",
+                    password: "password")
+
+    visit '/'
+    click_on "Login"
+    expect(current_path).to eq(login_path)
+
+    fill_in "email", with: user.email
+    fill_in "password", with: user.password
+    click_on "Submit"
+
+    expect(current_path).to eq('/')
+    expect(page).to have_content("Logout")
+
+    click_on "Logout"
+    expect(page).to have_content("Login")
+    expect(page).to have_content("Create Account")
+  end
+
 end
