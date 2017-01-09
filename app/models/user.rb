ab3676c9d3ea35eb4a532e87349f768332f8f871
SusiIrwin <susi.irwin@gmail.com>
2017-01-09 09:41:06 -0700

    User can create an account and Login with that account

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_secure_password
+  validates_confirmation_of :password, message: "Passwords Must Match."
 end
--- spec/features/user_can_create_login_spec.rb
+++ spec/features/user_can_create_login_spec.rb
@@ -1,20 +1,22 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe "Account Creation" do
-  it "allows a visitor to create an acount and log in" do
-    visit '/'
+RSpec.describe "can create login account", :js => :true do
+  scenario "allow visitor to create an account and log in" do
+    visit "/"
+
     click_on "Create Account"
     expect(current_path).to eq(signup_path)
 
-    fill_in "user_name", with: "Susi"
     fill_in "user_email", with: "email@email.com"
     fill_in "user_password", with: "password"
     fill_in "user_password_confirmation", with: "password"
 
-    click_on "Create My Account"
+    click_on "Submit"
 
     expect(current_path).to eq('/')
     expect(page).to have_content("Logout")
+    expect(page).to have_content("email@email.com")
     expect(page).to_not have_content("Create Account")
+
   end
 end
