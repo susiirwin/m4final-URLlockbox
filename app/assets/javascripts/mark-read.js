a403136376029ce22468c7a257131885d783274b
SusiIrwin <susi.irwin@gmail.com>
2017-01-09 15:49:05 -0700

    wip - troubleshoot connection

--- app/assets/javascripts/mark-read.js
+++ app/assets/javascripts/mark-read.js
@@ -8,16 +8,16 @@ $(document).ready(function(){
     var linkId = $this.parents('.link').data('id');
     var readLink = $(this).data("url")
 
-    $.ajax({
-      url: '/api/v1/links/' + linkId,
-      method: 'PATCH',
-      data: {read: true}
-    }).then(
+    // $.ajax({
+    //   url: '/api/v1/links/' + linkId,
+    //   method: 'PATCH',
+    //   data: {read: true}
+    // });
 
     $.ajax( {
       method: 'POST',
       data: {url: readLink},
       url: "http://si-m4-final-service.herokuapp.com/links"
-    }))
+    });
   })
 })
