5377422ddfecd022018d34d01ec9b72d788235fe
SusiIrwin <susi.irwin@gmail.com>
2017-01-09 16:25:53 -0700

    Update LINK

--- app/assets/javascripts/mark-read.js
+++ app/assets/javascripts/mark-read.js
@@ -3,13 +3,23 @@ var $newLinkTitle, $newLinkUrl;
 $(document).ready(function(){
 
   $('#links-list').on('click', 'button.mark-read', function(){
-    var $this = $(this);
+    console.log('win')
+    // var $this = $(this);
     var linkId = $this.parents('.link').data('id');
 
-    $.ajax({
-      url: '/api/v1/links/' + linkId,
-      method: 'PATCH',
-      data: {read: true}
-    });
+    // $.ajax({
+    //   url: '/api/v1/links/' + linkId,
+    //   method: 'PATCH',
+    //   data: {read: true}
+    // });
+  // })
+
+  var readLink = $(this).data("url")
+
+  // send the link to HotReads
+  $.ajax( {
+    method: 'POST',
+    data: {url: readLink},
+    url: "https://si-m4-final-service.herokuapp.com/links"
   })
 })
